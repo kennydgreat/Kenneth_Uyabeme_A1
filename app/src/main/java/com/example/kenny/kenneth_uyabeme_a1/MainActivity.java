1efb27153bd9480a592bae3b89df91f8ce0d1405
Kenneth Uyabeme <kuyabeme@gmail.com>
2017-09-18 15:33:14 -0400

    -clears all fields Clear button functionality implemented

--- app/src/main/java/com/example/kenny/kenneth_uyabeme_a1/MainActivity.java
+++ app/src/main/java/com/example/kenny/kenneth_uyabeme_a1/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.kenny.kenneth_uyabeme_a1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    //Fields
+    //FIELDS
+
     //-------------------------
     //Spinners and respective TextViews
     //-----------------
@@ -17,7 +21,21 @@ public class MainActivity extends Activity {
     TextView numOfPeopleTextView;
     //-----------------
 
+    //EditText
+    EditText enterAmount_editText;
 
+    //Button
+    Button clear_Button;
+
+
+    //Button listener Class
+    private class ClearButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            enterAmount_editText.setText("");
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +47,22 @@ public class MainActivity extends Activity {
         numOfPeopleSpinner = findViewById(R.id.numOfPeopleSpinner);
         numOfPeopleTextView = findViewById(R.id.numOfPeopleTextView);
 
+        //Getting reference to editText
+        enterAmount_editText = findViewById(R.id.enterAmount_editText);
+
+        //Getting reference to Button
+        clear_Button = findViewById(R.id.clear_Button);
+        ClearButtonListener buttonListener = new ClearButtonListener();
+        clear_Button.setOnClickListener(buttonListener);
+
         //String array containing spinner integer data
         String[] tipSpinnerData = getResources().getStringArray(R.array.TipSpinnerData);
         String[] numofPeopleSpinnerData = getResources().getStringArray(R.array.NumberOfPeopleSpinnerData);
 
 
         //Creating AraayAdapters
-        ArrayAdapter<String> tipSpinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tipSpinnerData);
-        ArrayAdapter<String> numofPplSpinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, numofPeopleSpinnerData);
+        ArrayAdapter<String> tipSpinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, tipSpinnerData);
+        ArrayAdapter<String> numofPplSpinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, numofPeopleSpinnerData);
         tipSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         numofPplSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
